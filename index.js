618baff9e65985e555bfa0fd32e19193b0c090e0
maxohsawa <max.ohsawa@gmail.com>
2021-08-01 08:05:01 -0400

    adds some links to navbar comp, adds navbar to index comp, renders index comp

---
+++ index.js
@@ -0,0 +1,11 @@
+function Spa() {
+
+    return (
+        <>
+            <h1>Welcome to Badbank</h1>
+            <Navbar/>
+        </>
+    );
+}
+
+ReactDOM.render(<Spa/>, document.getElementById('root'));
---
+++ navbar.js
@@ -0,0 +1,13 @@
+function Navbar() {
+
+    return (
+        <>
+            <a href="#/createaccount">Create Account</a>
+            <a href="#/login">Login</a>
+            <a href="#/deposit">Deposit</a>
+            <a href="#/withdraw">Withdraw</a>
+            <a href="#/balance">Balance</a>
+            <a href="#/alldata">All Data</a>
+        </>
+    );
+}
